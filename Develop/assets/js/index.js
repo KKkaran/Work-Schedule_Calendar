38de7a19b0a2179f632a167b540cd16b97a7b91f
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2021-11-16 02:45:30 -0500

    we are so screwed hashmaps not working as expected

--- Develop/assets/js/index.js
+++ Develop/assets/js/index.js
@@ -1,4 +1,7 @@
 var timeBlockContainer = $(".container")
+//creates hour and call createtimeblocks func to display markup
+let tasksHashMap = new Map();
+var listtasks = []
 function createBusinessHours(){
     for(var t=0;t<9;t++){
         var hour = t+9;
@@ -32,39 +35,50 @@ function createTimeBlocks(hour,am){
     slotContainer.append(timeStamp,eventEntry,updateBtn)
     timeBlockContainer.append(slotContainer)
 }
-
-$(".container").on("click",".evententry", function(){
+//local storage function
+function setLocalStorage(){
     
+    console.log(listtasks)
+    localStorage.setItem("tasks3",JSON.stringify(listtasks))
+}
+//on eventEnrty click,changes to textarea to edit
+$(".container").on("click",".evententry", function(){
     var text = $(this).
                 html().
                 trim()
-    
-    console.log(`the text value is: ${text}`)
     var textarea = $("<textarea>").
                     addClass("eventinput col-10").
                     val(text)
-
     $(this).replaceWith(textarea)
     textarea.trigger("focus")
 })
-
+//when clicked outside, it converts back to default p but wont save
+//until the corresponding button is clicked
 $(".container").on("blur","textarea", function(){
-    
     var text = $(this).
                 val().
                 trim()
-    
-    
     var eventEntry = $("<p>").
                     addClass("col-10 bg-danger evententry").
                     html(text)             
-
-    $(this).replaceWith(eventEntry)
-
-    console.log(text)
-    
+    $(this).replaceWith(eventEntry)    
 })
+$(".container").on("click","button",function(){
 
+    var key = $(this).closest("div").attr("id")
+    var value = $(this).closest("div").find(".evententry").html()
+    
+    //send it to local storage!!
+    tasksHashMap.set(key,value)
+    for (const [key, value] of tasksHashMap.entries()) {
+        var obj = {}
+        obj.id = key
+        obj.entry = value
+      }
+    listtasks.push(obj)
+    setLocalStorage()
+    //console.log(tasksHashMap.entries())
+})
 
 
 
@@ -87,3 +101,23 @@ createBusinessHours();
 
 //get's the current day and date
 $("#currentDay").html(moment().format('dddd, MMMM Do'))
+
+
+
+
+
+
+
+
+
+
+//*****************GOOD TO KNOW STUFF************************************************** */
+
+// document.querySelector(".container").addEventListener("click",function(e){
+
+//     if(e.target.matches("button")){
+//         console.log(e.target.closest("div").querySelector(".evententry").innerHTML)
+//     }
+// })
+
+
